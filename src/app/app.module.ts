40ee9253a8de28dc6aaca7ef2059f9f0a9e065c3
Thalles Conceição <thallesangelo@gmail.com>
2021-07-03 18:35:44 -0300

    Creates project, user card and list component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { UserCardComponent } from './user-card/user-card.component';
+import { UserListComponent } from './user-list/user-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserCardComponent,
+    UserListComponent
   ],
   imports: [
     BrowserModule
