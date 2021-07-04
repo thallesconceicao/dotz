c851b6f6c1ea820960e8cae3276e382e11dabf49
Thalles Conceição <thallesangelo@gmail.com>
2021-07-04 20:31:35 -0300

    Creates product list component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
+import { ProductModule } from './product/product.module';
 import { StructureModule } from './structure/structure.module';
 import { UserModule } from './user/user.module';
 
@@ -14,7 +15,8 @@ import { UserModule } from './user/user.module';
     BrowserModule,
     UserModule,
     NgbModule,
-    StructureModule
+    StructureModule,
+    ProductModule
   ],
   providers: [],
   bootstrap: [AppComponent]
